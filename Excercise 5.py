0a1580d885e6a12cd28c1da67c820e6f071d7ab7
hungbachquoc10-ai <hung.bachquoc10@gmail.com>
2026-01-16 22:08:13 +0700

    Add files via upload

---
+++ Excercise 5.py
@@ -0,0 +1,9 @@
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+c = float(input("Enter the third number: "))
+sum = a + b + c
+product = a * b * c
+average = sum / 3
+print(f'The sum of the three numbers is: {sum}')
+print(f'The product of the three numbers is: {product}')
+print(f'The average of the three numbers is: {average}')
