591eef392ec4764d21efdf9f038e785c6f5076d2
hungbachquoc10-ai <hung.bachquoc10@gmail.com>
2026-01-16 22:07:55 +0700

    Add files via upload

---
+++ Excercise 4.py
@@ -0,0 +1,6 @@
+lenght = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+perimeter = lenght + width + lenght + width
+print (f"The perimeter of the rectangle is {perimeter:.2f}")
+area = lenght * width
+print(f"The area of the rectangle with length {lenght} and width {width} is {area:.2f}")
